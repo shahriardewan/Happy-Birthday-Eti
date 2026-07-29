5b4623a868274a9df4a2341c239d80d5d3557765
shahriardewan <shahriar.s.dewan@gmail.com>
2026-07-29 13:20:01 +0600

    Refactor image paths and add new images
    Updated image paths to remove 'images/' prefix and added two new images.

--- script (3).js
+++ script (3).js
@@ -8,12 +8,14 @@ const music = document.getElementById("music");
 const startScreen = document.getElementById("startScreen");
 const startBtn = document.getElementById("startBtn");
 const images = [
-    "images/1.jpg",
-    "images/2.jpg",
-    "images/3.jpg",
-    "images/4.jpg",
-    "images/5.jpg",
-    "images/6.jpg"
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
+    "6.jpg",
+    "7.jpg",
+    "8.jpg"
 ];
 
 let current = 0;
